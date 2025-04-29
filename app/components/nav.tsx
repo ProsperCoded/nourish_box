6082ce99bf9be89ce71c94fc2586a184a295e26f
ProsperCoded <enweremproper@gmail.com>
2025-04-29 12:45:07 +0100

    Add missing Google icon image file to public assets

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,36 +1,39 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
-import user from '../assets/icons8-user-48.png'
-import Logo from '../assets/nourish_box_folder/Logo files/Logomark.svg'
-import Link from 'next/link';
+import user from "../assets/icons8-user-48.png";
+import Logo from "../assets/nourish_box_folder/Logo files/Logomark.svg";
+import Link from "next/link";
 const nav = () => {
   return (
     <div>
-      <div className='flex  justify-between p-6 items-center lg:hidden'>
-        <Image src={Logo} alt='Logo' className='w-[150px]' />
+      <div className="lg:hidden flex justify-between items-center p-6">
+        <Image src={Logo} alt="Logo" className="w-[150px]" />
         <button>=</button>
-
       </div>
       <div className="flex justify-center">
-      <div className=' hidden lg:w-11/12 lg:flex justify-between items-center p-10 pb-0 md:w-100 text-black font-inter'>
-        <Image src={Logo} alt='Logo' className='w-[150px]' />
-        <ul className='flex py-0 w-1/2 font-inter'>
-          <Link href="/"  className='px-4  font-medium text-xl'>Home</Link>
-          <Link href="/receipes" className='px-4 font-medium text-xl'>Recipe </Link>
-          <Link href="/about_us" className='px-4 font-medium text-xl'>About us</Link>
-          <Link href="/" className='px-4 font-medium text-xl'>Community</Link>
-
-        </ul>
-        <div className='flex items-center justify-end w-1/12'>
-
-        
-          <Image src={user} alt='user icon' width={30} height={30.11} />
+        <div className="hidden lg:flex justify-between items-center p-10 pb-0 md:w-100 lg:w-11/12 font-inter text-black">
+          <Image src={Logo} alt="Logo" className="w-[150px]" />
+          <ul className="flex py-0 w-1/2 font-inter">
+            <Link href="/" className="px-4 font-medium text-xl">
+              Home
+            </Link>
+            <Link href="/receipes" className="px-4 font-medium text-xl">
+              Recipe{" "}
+            </Link>
+            <Link href="/about_us" className="px-4 font-medium text-xl">
+              About us
+            </Link>
+            <Link href="/" className="px-4 font-medium text-xl">
+              Community
+            </Link>
+          </ul>
+          <div className="flex justify-end items-center w-1/12">
+            <Image src={user} alt="user icon" width={30} height={30.11} />
+          </div>
         </div>
       </div>
-      </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default nav
+export default nav;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
-import { Plus_Jakarta_Sans } from 'next/font/google'
-import './globals.css'
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
 
 // If loading a variable font, you don't need to specify the font weight
 const jakarta = Plus_Jakarta_Sans({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
-})
- 
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={`${jakarta.variable}`}>
       <body>{children}</body>
     </html>
-  )
+  );
 }
