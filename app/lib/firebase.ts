809ee1b2e68495d6143de1d6a45abfc3ff28080b
Wonuola <wonuolalonge@gmail.com>
2025-04-30 09:27:26 +0100

    api bug fix

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyBWOYoW3mard513BQSJGisWXbtWafyRBAc"  ,
+  apiKey: "AIzaSyBWOYoW3mard513BQSJGisWXbtWafyRBAc",
   authDomain: "nourish-box-6bb04.firebaseapp.com",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
