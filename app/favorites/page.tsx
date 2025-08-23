1f96a5c646e56d1334deeec5e30b5089bb7b6f4b
Prosper Coded <enweremproper@gmail.com>
2025-08-23 06:25:07 +0100

    Refactor favorites page to improve component organization and imports
    - Reorganized import statements for better readability and structure.
    - Ensured consistent usage of components such as LoginPrompt, RecipeList, and SearchBar.
    - Maintained functionality while enhancing code clarity.

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,19 +1,16 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { useFavorites } from "../contexts/FavContext";
-import { useRouter } from "next/navigation";
-import RecipeList from "../components/RecipeCard";
-import icon from "../assets/nourish_box_folder/Logo files/icon.svg";
-import search from "../assets/icons8-search-48.png";
-import { useAuth } from "../contexts/AuthContext";
-import Image from "next/image";
 import Link from "next/link";
-import SearchBar from "../components/Search_bar";
-import { LoginPromptWrapper } from "../components/LoginPromptWrapper";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import LoginPrompt from "../components/LoginPrompt";
+import { LoginPromptWrapper } from "../components/LoginPromptWrapper";
+import RecipeList from "../components/RecipeCard";
 import RecipeCardSkeleton from "../components/RecipeCardSkeleton";
+import SearchBar from "../components/Search_bar";
 import Nav from "../components/nav";
+import { useAuth } from "../contexts/AuthContext";
+import { useFavorites } from "../contexts/FavContext";
 
 interface Props {
   className?: string;
