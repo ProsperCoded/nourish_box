c45d8b91d87169e8dfdc5f7b9a43236557d10013
Wonuola <wonuolalonge@gmail.com>
2025-08-27 22:54:26 +0100

    feat/stylig fixes

--- app/shop/[id]/page.tsx
+++ app/shop/[id]/page.tsx
@@ -39,7 +39,7 @@ export default function RecipePage({ params }: PageProps) {
 
     if (isLoading) {
         return (
-            <div className="flex justify-center items-center min-h-[400px]">
+            <div className="flex justify-center items-center min-h-[400px] font-inter">
                 <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-orange-500"></div>
             </div>
         );
